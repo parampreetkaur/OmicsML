7c4c46b7584f771575ec48f453942312135aa790
parampreetkaur <parampreetkaur93@gmail.com>
2023-10-03 21:14:46 +0530

    Update survive_plot.R

--- survive_plot.R
+++ survive_plot.R
@@ -5,7 +5,7 @@ library(survcomp)
 
 data2<-read.csv("data/uploaded9.csv")
 size <- rexp(585,1)
-ddtr <- cbind(time = data2[, "time"], event = data2[, "status"], score = size)
+ddtr <- cbind(time = data2[, "survival_time"], event = data2[, "survival_status"], score = size)
 
 perf <- sbrier.score2proba(data.tr = data.frame(ddtr), data.ts = data.frame(ddtr), method = "cox")
 perf
