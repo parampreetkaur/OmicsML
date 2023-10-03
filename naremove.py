bcc8a5ff997e6de3568a277287874b5426778d8e
parampreetkaur <parampreetkaur93@gmail.com>
2023-10-03 20:54:26 +0530

    Update naremove.py

--- naremove.py
+++ naremove.py
@@ -1,12 +1,12 @@
 import pandas as pd
 def preprocess1():
   
-  dataset=pd.read_csv("data/uploaded10.csv",na_values='')
+  dataset=pd.read_csv("data/uploaded10.csv",na_values='')                      
   dataset.isnull().sum().sum()
   X=dataset.isnull().sum()
   dataset1=pd.DataFrame(dataset)
   perc = 10.0
   min_count =  int(((100-perc)/100)*dataset1.shape[0] + 1)
   mod_dataset = dataset1.dropna( axis=1,thresh=min_count)
-  pd.DataFrame(mod_dataset).to_csv("data/missing_drop_new.csv",index=False)
+  pd.DataFrame(mod_dataset).to_csv("data/missing_drop_integratednew.csv",index=False)
   return X
