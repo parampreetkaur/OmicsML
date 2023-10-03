b4d8e31ec193066829487fa4e3a3ae06d8463949
parampreetkaur <parampreetkaur93@gmail.com>
2023-10-03 21:10:48 +0530

    Update survive.R

--- survive.R
+++ survive.R
@@ -3,10 +3,10 @@ library(survminer)
 library(dplyr)
 library(survcomp)
 
-
 data2<-read.csv("data/uploaded9.csv")
 size <- rexp(585,1)
-ddtr <- cbind(time = data2[, "survival_time"], event = data2[, "survival_status"], score = size)
+ddtr <- cbind(time = data2[, "time"], event = data2[, "status"], score = size)
+
 
 perf <- sbrier.score2proba(data.tr = data.frame(ddtr), data.ts = data.frame(ddtr), method = "cox")
 perf
@@ -15,18 +15,18 @@ plot(x = perf$time, y = perf$bsc, xlab = "Time (days)", ylab = "Brier score", ty
 return (plot)
 
 
-perf1 <- concordance.index(x = size, surv.time = data2[,"survival_time"], surv.event = data2[, "survival_status"], method = "noether",na.rm = TRUE)
+perf1 <- concordance.index(x = size, surv.time = data2[,"time"], surv.event = data2[, "status"], method = "noether",na.rm = TRUE)
 print(perf1[1:5])
 return (perf1)
 
 
-
-perf2 <- D.index(x = size, surv.time = data2[,"survival_time"], surv.event = data2[, "survival_status"], na.rm = TRUE)
+perf2 <- D.index(x = size, surv.time = data2[,"time"], surv.event = data2[, "status"], na.rm = TRUE)
 print(perf2[1:6])
 
 
 
-perf3 <- hazard.ratio(x = size, surv.time = data2[,"survival_time"], surv.event = data2[, "survival_status"], na.rm = TRUE)
+perf3 <- hazard.ratio(x = size, surv.time = data2[,"time"], surv.event = data2[, "status"], na.rm = TRUE)
 print(perf3[1:6])
+
 
 
