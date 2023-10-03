ee504c0cf34234a6b44b2e08834bc2be95616164
parampreetkaur <parampreetkaur93@gmail.com>
2023-10-03 20:52:47 +0530

    Update anova.py

--- anova.py
+++ anova.py
@@ -11,14 +11,14 @@ def featurefxnanova():
   n=len(df.axes[1])
   df.head()
   df.groupby(df.columns[-1]).mean()
-
+ 
   y = df.iloc[:,(n-1)]    
   x = df.iloc[:,0:(n-1)]
- 
+
   from sklearn.model_selection import train_test_split
   x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
   from sklearn.feature_selection import SelectKBest, f_classif
-  selector = SelectKBest(f_classif, k=int(n/2))      
+  selector = SelectKBest(f_classif, k=int(n/2))     
   selector.fit(x_train, y_train)
   cols = selector.get_support(indices=True)
   
